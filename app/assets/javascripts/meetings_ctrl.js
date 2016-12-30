f0a1ded9a8fbe2ecc448b5df1b6f3ade88dd3bf4
Peter Jang <peter.x.jang@gmail.com>
2016-12-29 20:36:51 -0600

    Convert meetings index to angular

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -1,8 +1,11 @@
 /* global angular */
 (function() {
-  angular.module("app").controller("meetingsCtrl", function($scope) {
+  angular.module("app").controller("meetingsCtrl", function($scope, $http) {
     $scope.setup = function() {
-      $scope.message = "Hello from angular!";
+      $http.get('/api/v1/meetings').then(function(response) {
+        $scope.meetings = response.data;
+        console.log($scope.meetings);
+      });
     };
   });
 })();
