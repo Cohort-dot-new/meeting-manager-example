4e9710745e5a45dd03fb7343784d16d72ec4798c
Peter Jang <peter.x.jang@gmail.com>
2016-12-29 18:27:42 -0600

    Add meeting new action (incpmplete create)

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -8,6 +8,10 @@ class MeetingsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
   def show
     @meeting = Meeting.find_by(id: params[:id])
     render 'show.html.erb'
